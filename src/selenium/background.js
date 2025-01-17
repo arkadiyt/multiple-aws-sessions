072c20cba60e73d5ecedd569d0d1b388a9a6fd1d
arkadiyt <arkadiyt@users.noreply.github.com>
2025-01-17 01:42:30 +0000

    Auto formatted code

--- src/selenium/background.js
+++ src/selenium/background.js
@@ -2,12 +2,11 @@
  * This file is only included when the extension is built with `SELENIUM=1 make build` or `SELENIUM=1 npx webpack`
  * If the SELENIUM env var is not set (e.g. for production builds) then when webpack is resolving
  * selenium/background.js it will include an empty file instead
- * 
+ *
  * This file faciliates coverage testing when executing in a selenium environment
  */
 
-
 // Istanbul writes coverage data into window.__coverage__
-this.window = {}
+this.window = {};
 
 // TODO add the hook to export coverage data
